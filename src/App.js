a210680066475e432622dc8e0014b13bd668d794
saurav ranjan <sauravr@maveric-systems.com>
2020-08-07 10:26:53 +0530

    kjkjefjkwek

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const a = b + 2;
-  const c = d + 3;
-  const d = j + 3;
-  console.log("hhhhhhhhhhhhhhh");
-  console.log("kkkkkkkk");
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const a = b + 2;
+    const c = d + 3;
+    const d = hfhf + 3;
+    console.log('hhhhhhhhhhhhhhh');
+    console.log('kkkkkkkk');
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
